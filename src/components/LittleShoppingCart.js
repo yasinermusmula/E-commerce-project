4d6bfa5800930df39879e612290c40121ef226dd
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-04-07 19:15:15 +0300

    LittleShoppingCart edited with new css and logics. Dispatch methods implemented

--- src/components/LittleShoppingCart.js
+++ src/components/LittleShoppingCart.js
@@ -1,9 +1,14 @@
 import { useDispatch, useSelector } from "react-redux";
 import { deleteShoppingCard } from "../store/actions/shoppingCartAction";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 
-export default function LittleShoppingCart() {
+export default function LittleShoppingCart({ product }) {
   const shoppingCartSection = useSelector((store) => store.shoppingCart.cart);
+
   const dispatch = useDispatch();
+  console.log(shoppingCartSection);
 
   const shoppingCartRemove = (id) => {
     dispatch(deleteShoppingCard(id));
@@ -11,18 +16,37 @@ export default function LittleShoppingCart() {
 
   return (
     <>
-      {/*<div className="bg-white divide-y divide-gray-100 rounded-lg shadow w-24 dark:bg-gray-700 mt-8 h-20">*/}
-      {/*  <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">*/}
-      {shoppingCartSection.map((product) => (
-        <div className="w-8">
-          <p>sa</p>
-          <img src={product.images[0].url} />
-          <div>sa</div>
-          <button onClick={() => shoppingCartRemove(product.id)}>Remove</button>
+      <div className=" p-4">
+        <div key={product.id} className="mb-4 hover:bg-gray-50 ">
+          <div>
+            <div className="flex">
+              <img
+                src={product.product.images[0].url}
+                className="h-20 object-cover rounded mr-4"
+              />
+              <div>
+                <div>
+                  <p className="text-sm font-bold">{product.product.name}</p>
+                </div>
+                <div className="flex">
+                  <p>Price:</p>
+                  <p>${(product.product.price * product.count).toFixed(2)}</p>
+                </div>
+                <div className="flex">
+                  <p>Count:</p>
+                  <p>{product.count}</p>
+                </div>
+                <button
+                  onClick={() => shoppingCartRemove(product.product.id)}
+                  className="text-red-500 hover:text-red-700"
+                >
+                  <FontAwesomeIcon icon={faTimes} />
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-      ))}
-      {/*</ul>*/}
-      {/*</div>*/}
+      </div>
     </>
   );
 }

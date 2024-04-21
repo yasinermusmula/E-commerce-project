2fd4b65e8d842dc7e5179c08889027d54be6fd3e
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-04-21 17:08:14 +0300

    PaymentPage.js created

---
+++ src/components/PaymentPage.js
@@ -0,0 +1,103 @@
+import Hero from "../layout/Hero";
+import Footer from "./Footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+export default function PaymentPage() {
+  const shoppingCart = useSelector((store) => store.shoppingCart.cart);
+
+  let totalProductPrice = shoppingCart.reduce((count, item) => {
+    return count + item.count * item.product.price;
+  }, 0);
+  return (
+    <div className="w-full flex-wrap">
+      <Hero />
+      <div className="container mx-auto my-6 p-4 bg-gray-100 rounded-xl flex justify-between gap-10 flex-col">
+        <div className="flex items-center gap-3 text-xs text-gray-500 font-semibold mt-3">
+          <FontAwesomeIcon icon={faArrowLeft} />
+          <p className="cursor-pointer hover:text-gray-400">
+            Go Back To My Cart
+          </p>
+        </div>
+        <div className="flex w-full">
+          <div className="flex  md:flex-row gap-4 p-4">
+            <div className="flex flex-col">
+              <div className="flex gap-2 sm:flex-row">
+                <div className=" border-solid border-b-4 border-purple-50 mb-4 p-4 bg-white rounded shadow sm:w-1/2">
+                  <div className="flex justify-between gap-2">
+                    <h2 className="text-lg font-bold mb-3 ">
+                      Address Information
+                    </h2>
+                    <h1 className="text-2xl font-bold text-green-300">1</h1>
+                  </div>
+                </div>
+                <div className="p-4 mb-4 bg-white rounded shadow  sm:w-1/2">
+                  <div className="flex justify-between gap-2">
+                    <h2 className="text-lg font-bold mb-3w-1/2">
+                      Payment Options
+                    </h2>
+                    <h1 className="text-2xl font-bold text-green-300">2</h1>
+                  </div>
+                  <p className="text-sm text-gray-500">
+                    You can make your payment safely with a
+                    <span className="font-bold text-gray-400">
+                      bank/credit card
+                    </span>{" "}
+                    or{" "}
+                    <span className="font-bold text-gray-400">
+                      Shopping Credit
+                    </span>
+                    .
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-col mt-4 p-4 gap-3 bg-white rounded shadow">
+                <div className="flex justify-between w-full ">
+                  <h2 className="text-lg font-bold mb-3">Delivery Address</h2>
+                  <div className="flex items-center justify-between">
+                    <button className="p-2 bg-gray-200 rounded cursor-pointer hover:bg-gray-300 hover:border hover:border-solid hover:border-gray-100 ">
+                      <p className="flex text-xs font-bold text-gray-900 ">
+                        Add New Address +
+                      </p>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex h-[350px]">
+              <div className="rounded-xl border border-solid border-semiGrey p-4 w-96 lg:w-[400px] h-full justify-between flex flex-col bg-white">
+                <h3 className="text-2xl font-bold mb-4 text-center">
+                  Order Summary
+                </h3>
+                <div className="flex flex-col p-2 justify-center ">
+                  <div className="mb-4">
+                    <div className="flex justify-between mb-2 gap-2">
+                      <span className="font-bold">Total Products:</span>
+                      <span className="font-normal">
+                        {totalProductPrice.toFixed(2)}
+                      </span>
+                    </div>
+                  </div>
+                  <div className="flex justify-between mb-2 gap-2">
+                    <span className="font-bold">Shipping Total:</span>
+                    <span>$30</span>
+                  </div>
+                  <Link to="payment">
+                    <button
+                      className={`${totalProductPrice === 0 ? "bg-gray-600" : "bg-blue-500"} mt-4 w-full text-white font-bold py-2 px-4 rounded-xl  transition duration-300 ease-in-out place-self-end`}
+                      disabled={totalProductPrice === 0}
+                    >
+                      Create Order
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}

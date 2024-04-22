ea632539cdd290fd0513faf362872b0c38b3f41f
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-04-22 16:47:11 +0300

    Address form implemented

--- src/components/PaymentPage.js
+++ src/components/PaymentPage.js
@@ -4,12 +4,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 export default function PaymentPage() {
+  const [addressToogle, SetAddressToogle] = useState(false);
   const shoppingCart = useSelector((store) => store.shoppingCart.cart);
 
   let totalProductPrice = shoppingCart.reduce((count, item) => {
     return count + item.count * item.product.price;
   }, 0);
+
+  const handleToggle = () => {
+    SetAddressToogle(!addressToogle);
+  };
+
   return (
     <div className="w-full flex-wrap">
       <Hero />
@@ -20,11 +27,11 @@ export default function PaymentPage() {
             Go Back To My Cart
           </p>
         </div>
-        <div className="flex w-full">
-          <div className="flex  md:flex-row gap-4 p-4">
+        <div className="flex w-full justify-center">
+          <div className="flex md:flex-row gap-4 p-4">
             <div className="flex flex-col">
               <div className="flex gap-2 sm:flex-row">
-                <div className=" border-solid border-b-4 border-purple-50 mb-4 p-4 bg-white rounded shadow sm:w-1/2">
+                <div className=" border-solid border-b-4 border-blue-500 mb-4 p-4 bg-white rounded shadow sm:w-1/2">
                   <div className="flex justify-between gap-2">
                     <h2 className="text-lg font-bold mb-3 ">
                       Address Information
@@ -32,7 +39,7 @@ export default function PaymentPage() {
                     <h1 className="text-2xl font-bold text-green-300">1</h1>
                   </div>
                 </div>
-                <div className="p-4 mb-4 bg-white rounded shadow  sm:w-1/2">
+                <div className="p-4 mb-4 bg-white rounded shadow sm:w-1/2">
                   <div className="flex justify-between gap-2">
                     <h2 className="text-lg font-bold mb-3w-1/2">
                       Payment Options
@@ -57,16 +64,19 @@ export default function PaymentPage() {
                   <h2 className="text-lg font-bold mb-3">Delivery Address</h2>
                   <div className="flex items-center justify-between">
                     <button className="p-2 bg-gray-200 rounded cursor-pointer hover:bg-gray-300 hover:border hover:border-solid hover:border-gray-100 ">
-                      <p className="flex text-xs font-bold text-gray-900 ">
+                      <button
+                        onClick={handleToggle}
+                        className="flex text-xs font-bold text-gray-900 "
+                      >
                         Add New Address +
-                      </p>
+                      </button>
                     </button>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="flex h-[350px]">
-              <div className="rounded-xl border border-solid border-semiGrey p-4 w-96 lg:w-[400px] h-full justify-between flex flex-col bg-white">
+            <div className="flex h-[250px]">
+              <div className="rounded-xl border border-solid border-gray-100 p-4 w-96 lg:w-[400px] h-full justify-between flex flex-col bg-white">
                 <h3 className="text-2xl font-bold mb-4 text-center">
                   Order Summary
                 </h3>
@@ -94,6 +104,82 @@ export default function PaymentPage() {
                 </div>
               </div>
             </div>
+            {addressToogle && (
+              <div
+                className={`fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50`}
+              >
+                <div className="bg-white p-4 rounded max-w-lg w-full">
+                  <form className="space-y-4 ">
+                    <div className="flex">
+                      <h2 className="items-start">Add New address</h2>
+                      <button className="ml-80">x</button>
+                    </div>
+                    <div className="flex-col flex">
+                      <label>Adress Title:</label>
+                      <input
+                        placeholder="Enter an Adress title (e,g, Home, Work)"
+                        className="border rounded border-gray-500 w-full"
+                      />
+                      <div className="flex justify-between">
+                        <div className="flex-col flex">
+                          <label>Name:</label>
+                          <input
+                            placeholder="name"
+                            className="border rounded border-gray-500"
+                          />
+                        </div>
+                        <div className="flex flex-col">
+                          <label>Surname:</label>
+                          <input
+                            placeholder="Surname"
+                            className="border rounded border-gray-500"
+                          />
+                        </div>
+                      </div>
+                      <label>Phone:</label>
+                      <input
+                        placeholder="+905........"
+                        className="border rounded border-gray-500 w-full"
+                      />
+                      <label>City:</label>
+                      <select
+                        name="city"
+                        className="border border-gray-500 w-36"
+                      >
+                        <option>Select a City</option>
+                        <option value="1">Adana</option>
+                      </select>
+                      <label>District:</label>
+                      <select
+                        name="city"
+                        className="border border-gray-500 w-36"
+                      >
+                        <option>Select a District</option>
+                        <option value="1">Adana</option>
+                      </select>
+                      <label>Neighbourhood:</label>
+                      <input
+                        placeholder="Enter your Neighbourhood"
+                        className="border rounded border-gray-500 w-full"
+                      />
+                      <label>Address:</label>
+                      <textarea
+                        placeholder="Enter your Adress(Street,Apertment,Home number)"
+                        className="border rounded border-gray-500 w-full h-20"
+                      />
+                    </div>
+                    <div className="justify-center flex">
+                      <button className="rounded mx-2 bg-blue-500 w-28 h-8 font-montserrat text-gray-100 font-medium text-sm">
+                        Save address
+                      </button>
+                      <button className="mx-2 rounded bg-gray-300 w-20 text-sm font-montserrat">
+                        Cancel
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
